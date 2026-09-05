8d53f993a9728f51d98c6ab28c0c8f7429a384c9
matar mbow <mbowmatar00@gmail.com>
2026-09-06 03:03:13 +0800

    fix: copy prisma to next/node_modules in vercel install

--- next/next.config.js
+++ next/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@jambarrtech/shared'],
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client']
-  },
-  serverExternalPackages: ['@prisma/client'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' }
